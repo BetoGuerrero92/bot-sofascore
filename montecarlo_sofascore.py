c2a423da0539269b54d6d17d08bf5d1c8b0f955f
BetoGuerrero92 <humbertogemini92@gmail.com>
2026-09-22 05:47:01 -0600

    Update montecarlo_sofascore.py

--- montecarlo_sofascore.py
+++ montecarlo_sofascore.py
@@ -28,7 +28,8 @@ HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
     "Accept": "/",
     "Accept-Language": "es-ES,es;q=0.9,en;q=0.8",
-    "Referer": "https://www.sofascore.com/"
+    "Referer": "https://www.sofascore.com/",
+    "Origin": "https://www.sofascore.com"
 }
 
 SIMULATIONS = 100_000
