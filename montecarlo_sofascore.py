5138923e336ad16572b34ccd9570a93d22030a5d
BetoGuerrero92 <humbertogemini92@gmail.com>
2026-09-22 19:06:25 -0600

    Update montecarlo_sofascore.py

--- montecarlo_sofascore.py
+++ montecarlo_sofascore.py
@@ -464,9 +464,3 @@ def index():
 def webhook():
     return procesar_webhook_telegram(request)
 
-# =========================================================
-# PUNTO DE ENTRADA PRINCIPAL
-# =========================================================
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-   app.run(host="0.0.0.0", port=port)
