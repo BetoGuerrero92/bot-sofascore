c279dc7efae38c9c70b8a7d4a754a7bdca1176c6
BetoGuerrero92 <humbertogemini92@gmail.com>
2026-09-22 18:59:22 -0600

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- montecarlo_sofascore.py
+++ montecarlo_sofascore.py
@@ -467,6 +467,6 @@ def webhook():
 # =========================================================
 # PUNTO DE ENTRADA PRINCIPAL
 # =========================================================
-if _name_ == "_main_":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
    app.run(host="0.0.0.0", port=port)
